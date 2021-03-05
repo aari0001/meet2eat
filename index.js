53f43470160a3ba5ca5c370d9d567e6a101bd3da
aari0001 <31092626+aari0001@users.noreply.github.com>
2021-03-05 23:04:59 +1100

    Basic returns of nearby restaurants

--- index.js
+++ index.js
@@ -1,58 +1,84 @@
-let locationCentre;
-let locationCentreMarker;
-let service;
-let map;
+let locationCentre; // eg locationCentre.lat()
+let nearbyPlaces20; // 20 nearby restaurants
+let mainMap; // main google map
+let placeType = 'restaurant'; // type of places to look up nearby
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  mainMap = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: { lat: -37.812745, lng: 144.960566 },
   });
   const geocoder = new google.maps.Geocoder();
   document.getElementById("submit").addEventListener("click", () => {
-    markAddress(geocoder, map);
+    markAndList(geocoder);
   });
 }
 
-function markAddress(geocoder, resultsMap) {
+function markAndList(geocoder) {
+  // Function: Marks the location set by user and then lists nearby places
   const address = document.getElementById("address").value;
   geocoder.geocode({ address: address }, (results, status) => {
     if (status === "OK") {
       locationCentre = results[0].geometry.location;
-      resultsMap.setCenter(locationCentre);
+      mainMap.setCenter(locationCentre);
       locationCentreMarker = new google.maps.Marker({
-        map: resultsMap,
+        map: mainMap,
         position: locationCentre,
       });
-      var allNearbyRestaurants = listNearbyRestaurants(resultsMap);
+      nearbyPlaces20 = listNearby();
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
   });
 }
 
-function listNearbyRestaurants(resultsMap) {
+function listNearby() {
+  // Function: list 20 nearby restaurants, either based on sorted increasing distance or if in 5km radius
+  // Option 1: Use the following if you want to get some random 20 restaurants within the set radius
   var request = {
     location: locationCentre,
-    radius: '5000',
-    type: ['restaurant']
+    radius: '5000', // in metres
+    type: [placeType]
   };
 
-  service = new google.maps.places.PlacesService(resultsMap);
-  return service.nearbySearch(request, callback);
+  // // Option 2: Use the following if you want to get 20 nearest restaurants
+  // var request = {
+  //   location: locationCentre,
+  //   rankBy: google.maps.places.RankBy.DISTANCE,
+  //   type: ['restaurant']
+  // };
+
+  var nearbySearchService = new google.maps.places.PlacesService(mainMap);
+  return nearbySearchService.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
+  // Function: returns results of nearby places
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     console.log(results.length);
     for (var i = 0; i < results.length; i++) {
-      console.log(haversine_distance(locationCentre, results[i].geometry.location))
+      console.log(haversine_distance(locationCentre, results[i].geometry.location));
+      // createMarker(results[i]); // mark the results
     }
     return results;
   }
 }
 
+function createMarker(place) {
+  // Function: Draws a marker of the 'place' on the map
+  if (!place.geometry || !place.geometry.location) return;
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
+
 function haversine_distance(mk1, mk2) {
+  // Function: Finds distance in miles LOL, src: https://cloud.google.com/blog/products/maps-platform/how-calculate-distances-map-maps-javascript-api
   var R = 3958.8; // Radius of the Earth in miles
   var rlat1 = mk1.lat() * (Math.PI/180); // Convert degrees to radians
   var rlat2 = mk2.lat() * (Math.PI/180); // Convert degrees to radians
