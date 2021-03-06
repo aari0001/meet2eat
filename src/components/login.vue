a065f476d81a8b1fca9e77d226854d4711c7d402
James <git@gutenmorganfreeman.com>
2021-03-06 12:09:29 +1100

    updating to try and get css working

---
+++ src/components/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col items-center p-12">
+        <h1 class="text-4xl " >
+            meet2eat
+        </h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="w-screen h-screen bg-red-400">
-
+        <login v-if="pageSelected === 'login'"/>
     </div>
 </template>
 
 <script>
+
+    import Login from "./login";
     export default {
-        name: "mainPage.vue"
+        name: "mainPage.vue",
+        components: {Login},
+        computed: {
+            pageSelected() {
+                return this.$store.getters.getPageSelected
+            }
+        }
     }
 </script>
 
