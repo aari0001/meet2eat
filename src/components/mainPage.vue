0f36516aeca9ce35fed85db97c409608fd627140
James <git@gutenmorganfreeman.com>
2021-03-06 11:06:42 +1100

    vue and tailwind now work!

---
+++ src/components/mainPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-screen h-screen bg-red-400">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "mainPage.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
