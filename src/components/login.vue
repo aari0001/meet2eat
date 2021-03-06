df38d71a2a57803575835385b659ed7c65b546db
James <git@gutenmorganfreeman.com>
2021-03-06 12:31:32 +1100

    updating to try and get css working

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,20 @@
 <template>
-    <div class="flex flex-col items-center p-12">
-        <h1 class="text-4xl " >
+    <div class="grid grid-cols-4 gap-4 grid-rows-6 h-full w-full">
+        <h1 class="text-4xl col-start-2 col-span-2 mx-auto row-start-2" >
             meet2eat
         </h1>
-
+        <div class="row-start-3 col-span-2 col-start-2 h-full">
+            <label for="join" class="border-2 flex flex-row border-gray-300 rounded bg-gray-100">
+                <input id="join" type="text" class="bg-gray-100 px-2 py-1 rounded" placeholder="enter room code">
+                <button class="border-l-2 border-gray-300 p-1 bg-gray-300"> join</button>
+            </label>
+        </div>
+        <div class="row-start-5 col-span-2 col-start-2 h-full">
+            <label for="create" class="border-2 flex flex-row border-gray-300 rounded bg-gray-100">
+                <input id="create" type="text" class="bg-gray-100 px-2 py-1 rounded" placeholder="enter room code">
+                <button class="border-l-2 border-gray-300 p-1 bg-gray-300"> create</button>
+            </label>
+        </div>
     </div>
 </template>
 
