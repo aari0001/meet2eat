493715b3a6d975a70df1141c19704bade098e6e0
James <git@gutenmorganfreeman.com>
2021-03-06 12:09:55 +1100

    updating to try and get css working

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    pageSelected: 'login',
+  },
+  getters: {
+    getPageSelected(state) {
+      return state.pageSelected
+    }
+  },
+  mutations: {
+    setPageSelected(state, newPage) {
+      state.pageSelected = newPage
+    }
+
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
